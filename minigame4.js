9909f107f727a038b99cf72841d244272f74caa2
Amvina Pham <215578691+AmvP100@users.noreply.github.com>
2025-08-28 16:55:22 -0700

    added additional code for minigame4

--- minigame4.js
+++ minigame4.js
@@ -7,7 +7,14 @@ function safeLink(roundNumber) {
   document.getElementById("round2").style.display = "none";
   document.getElementById("round3").style.display = "none";
 
-  document.getElementById("round" + roundNumber).style.display = "block"; //if the user answers correctly, the next round displays
+  if (roundNumber) {
+    //if the user answers correctly, the next round displays
+    document.getElementById("round" + roundNumber).style.display = "block";
+  } else {
+    //no more rounds
+    document.getElementById("finalBoss").style.display = "block";
+    document.getElementById("textStatus").style.display = "none";
+  } 
 }
 
 function badLink() {
